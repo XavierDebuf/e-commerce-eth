e0c9afc41beb161d022dfc72262015b12075c009
XavierDebuf <x.debuf@gmail.com>
2023-09-01 14:39:55 +0200

    ajout de fichier jsx

---
+++ src/components/Games.jsx
@@ -0,0 +1,25 @@
+import React, { useState, useEffect } from "react";
+import Card from "./Card";
+
+const Games = () => {
+  const [Games, setGames] = useState([]);
+
+  useEffect(() => {
+    // Récupère les Games
+    fetch("../data/data.json")
+      .then((response) => response.json())
+      .then((Games) => setGames(Games));
+  }, []);
+
+  return (
+    <div>
+      <h1>Liste des Games</h1>
+
+      {Games.map((Jeux) => (
+        <Card Jeux={Jeux.url} name={Jeux.name} price={Jeux.price} description={Jeux.description} />
+      ))}
+    </div>
+  );
+};
+
+export default Games;
